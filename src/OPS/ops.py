971cb46ff9507cdf374021b03b8a725844b10eb4
thecreativeuniverse <me@creativeuniverse.dev>
2023-11-17 11:53:48 +0000

    add readme back to ops branch lol

--- src/OPS/ops.py
+++ src/OPS/ops.py
@@ -4,8 +4,8 @@ from geometry_msgs.msg import Pose
 class OPS:
     def __init__(self):
         rospy.Subscriber("simple_map", list, self.simple_map_callback)
-        rospy.Subscriber("estimated_pose", Pose, self.estimated_pose_callback)
-        rospy.Subscriber("known_objects", dict, self.known_objects_callback)
+        rospy.Subscriber("estimated_pose", Pose, self.estimated_pose_callback) 
+        rospy.Subscriber("known_objects", dict, self.known_objects_callback) # [("name", (x,y)),("name", (x,y))]
         rospy.Subscriber("target_object", str, self.target_object_callback)
 
         rospy.publisher("goal_position", Pose)
@@ -17,23 +17,24 @@ class OPS:
 
     def simple_map_callback(self, simple_map):
         self.simple_map = simple_map
-        self.long_term_goal()
+        self.calculate_long_term_goal()
         return None
 
     def estimated_pose_callback(self, estimated_pose):
         self.estimated_pose = estimated_pose
-        self.long_term_goal()
+        self.calculate_long_term_goal()
         return None
 
     def known_objects_callback(self, known_objects):
         self.known_objects = known_objects
-        self.long_term_goal()
+        self.calculate_long_term_goal()
         return None
 
     def target_object_callback(self, target_object):
         self.target_object = target_object
-        self.long_term_goal()
+        self.calculate_long_term_goal()
         return None
 
-    def long_term_goal(self):
+    def calculate_long_term_goal(self):
+
         return None
