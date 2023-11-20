b80414d96d7dc642d3f035e628c869dc92ffd4cb
thecreativeuniverse <me@creativeuniverse.dev>
2023-11-20 13:32:21 +0000

    rename ops

--- src/ops/srg.py
+++ src/ops/srg.py
@@ -6,18 +6,26 @@ class SRG:
     # dummy srg for now; will need a way to serialise/deserialise this later on.
     def __init__(self):
         objects = ["oven", "kettle", "toaster", "cushion", "sofa", "bed"]
-        self.srg = {}
+        self._srg = {}
         for obj in objects:
             object_dict = {}
             for temp in objects:
                 if temp == obj:
-                    object_dict.update({temp: 0})
-                elif temp in self.srg:
-                    object_dict.update({temp: self.srg.get(temp).get(obj)})
+                    object_dict.update({temp: 1})
+                elif temp in self._srg:
+                    object_dict.update({temp: self._srg.get(temp).get(obj)})
                 else:
                     object_dict.update({temp: np.random.uniform(0, 250)})
-            self.srg.update({obj: object_dict})
-
+            self._srg.update({obj: object_dict})
 
     def get_distance(self, obj, target):
-        return self.srg.get(obj).get(target)
+        return self._srg.get(obj).get(target)
+
+
+class TrainingSRG(SRG):
+
+    def __init__(self):
+        SRG.__init__(self)
+
+
+    def update_weights(self, data_idk):
