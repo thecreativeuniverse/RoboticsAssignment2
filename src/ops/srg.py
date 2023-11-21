e6f9cdd2a90c191b263be7394330e916b3ee2991
Ellie-Anne <ellieanne.watts@gmail.com>
2023-11-21 14:16:06 +0000

    srg now has file handling

--- src/ops/srg.py
+++ src/ops/srg.py
@@ -1,29 +1,52 @@
 import numpy as np
-
+import os
+import json
 
 class SRG:
 
     # dummy srg for now; will need a way to serialise/deserialise this later on.
-    def __init__(self):
-        objects = ["oven", "kettle", "toaster", "cushion", "sofa", "bed"]
-        self._srg = {}
-        dist, var, n = 100, 10, 1
-        for obj in objects:
-            object_dict = {}
-            for temp in objects:
-                if temp == obj:
-                    object_dict.update({temp: (0, 0, 0)})
-                elif temp in self._srg:
-                    object_dict.update({temp: self._srg.get(temp).get(obj)})
-                else:
-                    object_dict.update({temp: (dist, var, n)})
-            self._srg.update({obj: object_dict})
+    def __init__(self, filename=None):
+        if filename == None:
+            objects = ["oven", "kettle", "toaster", "cushion", "sofa", "bed"]
+            self._srg = {}
+            dist, var, n = 100, 10, 1
+            for obj in objects:
+                object_dict = {}
+                for temp in objects:
+                    if temp == obj:
+                        object_dict.update({temp: (0, 0, 0)})
+                    elif temp in self._srg:
+                        object_dict.update({temp: self._srg.get(temp).get(obj)})
+                    else:
+                        object_dict.update({temp: (dist, var, n)})
+                self._srg.update({obj: object_dict})
+            self.save_in_file()
+        else:
+            try:
+                file = open(filename, "r")
+                self._srg = json.load(file)
+            except IOError:
+                "Error: Could not find file"
 
     def get_distance(self, obj, target):
         return self._srg.get(obj).get(target)[0]
 
     def get_target_distribution(self, target):
         return self._srg.get(target)
+
+    def save_in_file(self):
+        try:
+            output_dir = os.path.join(os.path.dirname(__file__), "out")
+            os.makedirs(output_dir, exist_ok=True)
+            filename = 'srg.json'
+            #file = open(os.path.join(output_dir, filename), 'wb')
+
+            with open(os.path.join(output_dir, filename), 'w') as file:
+                json.dump(self._srg, file)
+
+            #json.dump(self._srg, file)
+        except IOError:
+            print("Error: could ot create file")
 
 
 class TrainingSRG(SRG):
@@ -45,3 +68,7 @@ class TrainingSRG(SRG):
         obj2_dict.update({obj1: (mean, var, n + 1)})
         srg.update({obj2: obj2_dict})
         self._srg = srg
+
+if __name__ == "__main__":
+    srg = SRG()
+    srg2 = SRG("../out/srg.json")
