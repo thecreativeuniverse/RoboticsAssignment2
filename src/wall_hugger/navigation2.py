1b300627a5de4efc88cf139dd03cfea2f598f3b7
BrightPurpleFin <BrightPurpleFin@hotmail.com>
2023-11-29 14:14:32 +0000

    restructured code to match coding style

--- src/wall_hugger/navigation2.py
+++ src/wall_hugger/navigation2.py
@@ -7,11 +7,11 @@ from nav_msgs.msg import Odometry
 from tf.msg import tfMessage
 from copy import deepcopy
 
-objectThreshold = 10
-thresholdMet = False
-odomRecent = []
-cmdPub = rospy.Publisher("cmd_vel", Twist, queue_size=100)
-odomPub = rospy.Publisher("estimated_pose", PoseStamped, queue_size=1)
+object_threshold = 10
+threshold_met = False
+odom_recent = []
+cmd_pub = rospy.Publisher("cmd_vel", Twist, queue_size=100)
+odom_pub = rospy.Publisher("estimated_pose", PoseStamped, queue_size=1)
 
 
 def callback(msg):
@@ -21,56 +21,56 @@ def callback(msg):
 
     # TODO: Prioritise entering nearby doorways.
 
-    if not thresholdMet:
-        discoveryAlgorithm(base_data, averages)
+    if not threshold_met:
+        discovery_algorithm(base_data, averages)
 
 
-def discoveryAlgorithm(base_data, averages):
+def discovery_algorithm(base_data, averages):
     # Crash recovery first
     if averages[0] < 0.45 or averages[1] < 0.4 or averages[2] < 0.4:
         base_data.linear.x = -1
-        cmdPub.publish(base_data)
+        cmd_pub.publish(base_data)
         rospy.sleep(0.1)
         base_data.linear.x = 0
         base_data.angular.z = 0.5
-        cmdPub.publish(base_data)
+        cmd_pub.publish(base_data)
         rospy.sleep(0.1)
 
     elif averages[3] < 0.4 or averages[4] < 0.45:
         base_data.linear.x = -1
-        cmdPub.publish(base_data)
+        cmd_pub.publish(base_data)
         rospy.sleep(0.1)
         base_data.linear.x = 0
         base_data.angular.z = -0.5
-        cmdPub.publish(base_data)
+        cmd_pub.publish(base_data)
         rospy.sleep(0.1)
 
     elif averages[2] >= 0.4:
         base_data.linear.x = 0.5
         base_data.angular.z += (random.random() * 0.5) - 0.25
 
-    elif (abs(odomRecent[0].pose.pose.position.x - odomRecent[1].pose.pose.position.x) <= 0.01) & (
-            abs(odomRecent[0].pose.pose.position.y - odomRecent[1].pose.pose.position.y) <= 0.01):
+    elif (abs(odom_recent[0].pose.pose.position.x - odom_recent[1].pose.pose.position.x) <= 0.01) & (
+            abs(odom_recent[0].pose.pose.position.y - odom_recent[1].pose.pose.position.y) <= 0.01):
         base_data.linear.x = -0.5
         base_data.angular.z += (random.random() * 0.5) - 0.25
 
     else:
         base_data.linear.x = -1
 
-    cmdPub.publish(base_data)
+    cmd_pub.publish(base_data)
 
 
 def odom_callback(msg):
-    global odomRecent
-    if len(odomRecent) < 5:
-        odomRecent.append(msg)
+    global odom_recent
+    if len(odom_recent) < 5:
+        odom_recent.append(msg)
     else:
-        odomRecent.pop(0)
-        odomRecent.append(msg)
+        odom_recent.pop(0)
+        odom_recent.append(msg)
 
 
 def tf_callback(msg):
-    odom = deepcopy(odomRecent[0])
+    odom = deepcopy(odom_recent[0])
 
     if (msg.transforms[0].child_frame_id != "odom" or msg.transforms[0].header.frame_id != "map"):
         return
@@ -82,19 +82,19 @@ def tf_callback(msg):
     odom.pose.pose.position.y += translation.y
     odom.pose.pose.position.z += translation.z
 
-    odomOrientation = [odom.pose.pose.orientation.w, odom.pose.pose.orientation.x, odom.pose.pose.orientation.y, odom.pose.pose.orientation.z]
+    odom_orientation = [odom.pose.pose.orientation.w, odom.pose.pose.orientation.x, odom.pose.pose.orientation.y, odom.pose.pose.orientation.z]
     rotation = [rotation.w, rotation.x, rotation.y, rotation.z]
-    orientation = quaternion_multiply(odomOrientation, rotation)
-    odom.pose.pose.orientation.w = orientation[0]
-    odom.pose.pose.orientation.x = orientation[1]
-    odom.pose.pose.orientation.y = orientation[2]
-    odom.pose.pose.orientation.z = orientation[3]
+    estimated_orientation = quaternion_multiply(odom_orientation, rotation)
+    odom.pose.pose.orientation.w = estimated_orientation[0]
+    odom.pose.pose.orientation.x = estimated_orientation[1]
+    odom.pose.pose.orientation.y = estimated_orientation[2]
+    odom.pose.pose.orientation.z = estimated_orientation[3]
 
-    poseStamped = PoseStamped()
-    poseStamped.header.frame_id = "map"
-    poseStamped.header.stamp = rospy.Time.now()
-    poseStamped.pose = odom.pose.pose
-    odomPub.publish(poseStamped)
+    estimated_pose = PoseStamped()
+    estimated_pose.header.frame_id = "map"
+    estimated_pose.header.stamp = rospy.Time.now()
+    estimated_pose.pose = odom.pose.pose
+    odom_pub.publish(estimated_pose)
 
 # Code taken from https://automaticaddison.com/how-to-multiply-two-quaternions-together-using-python/ 29/11/23 12:32pm GMT
 def quaternion_multiply(Q0,Q1):
@@ -118,9 +118,9 @@ def quaternion_multiply(Q0,Q1):
     return final_quaternion
 
 def known_objects_callback(msg):
-    if len(eval(msg.data)) >= objectThreshold:
-        global thresholdMet
-        thresholdMet = True
+    if len(eval(msg.data)) >= object_threshold:
+        global threshold_met
+        threshold_met = True
 
 
 def listener():
